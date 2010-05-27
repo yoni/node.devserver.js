64ad0bcabdbfd7e264c8a2d914a34bff5e57b682
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2010-05-26 23:43:28 -0400

    * Fixed lots of issues caused by node API changes.  * Added a closure to address the issue raised in: http://stackoverflow.com/questions/1875754/node-js-callback-getting-unexpected-value-for-variable  * A bunch of formatting changes.

--- devserver.js
+++ devserver.js
@@ -1,16 +1,17 @@
+var fs    = require('fs'),
+    sys   = require('sys'),
+    spawn = require('child_process').spawn;
 
-var posix = require('posix');
-var sys = require('sys');
 var server;
 var child_js_file = process.ARGV[2];
 var current_dir = __filename.split('/');
 current_dir = current_dir.slice(0, current_dir.length-1).join('/');
 
 var start_server = function(){
-    server = process.createChildProcess('node', [child_js_file]);
-    var pass_along = function(data){
+    server = spawn('node', [child_js_file]);
+    function pass_along(data){
         if (data !== null) sys.print(data);
-    };
+    }
     server.addListener('output', pass_along);
     server.addListener('error', pass_along);
     sys.puts('server started');
@@ -18,27 +19,34 @@ var start_server = function(){
 
 var restart_server = function(){
     sys.puts('change discovered, restarting server');
-    server.close();
+    server.kill('SIGHUP');
     setTimeout(start_server, 500);
 };
 
-var parse_file_list = function(dir, files){
-    for (var i=0;i<files.length;i++){
+function parse_file_list (dir, files) {
+
+    for (var i = 0; i < files.length; i++) {
+        var file = dir + '/' + files[i];
         (function(){
-            var file = dir+'/'+files[i];
-            posix.stat(file).addCallback(function(stats){
+            var file_for_callback = file;
+            fs.stat(file, function (err, stats) {
+                if(err) throw 'Could not load file stat for ' + file + '. ' + err;
                 if (stats.isDirectory())
-                    posix.readdir(file).addCallback(function(files){
-                        parse_file_list(file, files);
+                    fs.readdir(file_for_callback, function (err, files) {
+                        if(err) throw 'Could not read file ' + file + '. ' + err; 
+                        parse_file_list(file_for_callback, files);
                     });
                 else if (stats.isFile())
-                    process.watchFile(file, restart_server);
+                    fs.watchFile(file_for_callback, restart_server);
             });
         })();
     }
-};
 
-posix.readdir(current_dir).addCallback(function(files){
+}
+
+sys.puts(fs.readdir);
+fs.readdir(current_dir, function (err, files) {
+    if(err) throw 'Could not load files in the current directory. ' + err;
     parse_file_list(current_dir, files);
 });
 
